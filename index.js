b497cf0b2fb4a156239b6aba78caf170ae076007
CEllis <tehwavy@gmail.com>
2017-06-02 12:06:05 +1000

    changed to ES6 syntax

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-var express = require('express');
-var socket = require('socket.io');
+'use strict';
+
+const express = require('express');
+const socket = require('socket.io');
 
 // App setup
-var app = express();
-var port = 4000;
-var server = app.listen(port, function() {
-  console.log('Listening to port ' + port);
-});
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // Static files
 app.use(express.static('public'));
 
 // Socket setup and pass server
-var io = socket(server);
+const io = socket(server);
 io.on('connection', (socket) => {
-  console.log('Made socket connection', socket.id);
+  console.log(`Made socket connection ${ socket.id }`);
 
   // Handle chat event
-  socket.on('chat', function(data) {
-    io.sockets.emit('chat', data);
-  });
+  socket.on('chat', (data) => io.sockets.emit('chat', data));
 
   // Handle typing event
-  socket.on('typing', function(data) {
-    socket.broadcast.emit('typing', data);
-  });
+  socket.on('typing', (data) => socket.broadcast.emit('typing', data));
 });
