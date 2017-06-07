4c69d2fcc664ba3f25fc9badb568ef3b8ec48a10
CEllis <tehwavy@gmail.com>
2017-06-08 09:51:06 +1000

    added dependencies

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 'use strict';
 
+// Set app dependencies
 const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
 const socket = require('socket.io');
 
+// Set app components
+//const routes = require('./app/routes');
+//const session = require('./app/session');
+//const passport = require('./app/auth');
+//const ioServer = require('./app/socket')(app);
+
 // App setup
-const app = express();
 const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
